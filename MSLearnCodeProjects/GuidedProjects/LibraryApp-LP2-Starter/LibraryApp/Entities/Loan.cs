d69bc2868fb5fdb2f4ca9aa4f2ed454b8bcf841f
Chris Howd <cahowd@microsoft.com>
2024-06-25 15:36:12 -0700

    adding LP2 starter code project

---
+++ MSLearnCodeProjects/GuidedProjects/LibraryApp-LP2-Starter/LibraryApp/Entities/Loan.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryApp.Entities;
+public class Loan
+{
+    public int Id { get; set; }
+    public int BookItemId { get; set; }
+    public int PatronId { get; set; }
+    public DateTime LoanDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+    public BookItem? BookItem { get; set; }
+}
